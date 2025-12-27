7b8aa93a1b639cc62b4bc4a7ee047639dae87959
Rajaryan singh <raj.aryan2518@gmail.com>
2025-12-27 16:03:52 +0530

    socket connection

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+import { Socket } from 'dgram'
 import express from 'express'
+import http from 'http'
+import path from 'path'
+import {Server} from 'socket.io'
 
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-   res.send("Hello world")
+
+
+io.on('connection', (client) => {
+    console.log('A new user connected', client.id)
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+
+
+app.use(express.static(path.resolve('./public')))
+
+app.get('/', (req, res) => {
+   return res.sendFile('./public/index.html')
+})
+
+
+
+server.listen(PORT, () => {
+    console.log("server is rinning")
 })
